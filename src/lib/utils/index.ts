14cbf497c6947e3c34585dcc0de2ba3094f95da4
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-03-19 14:26:03 +0530

    fix not replacing all - and _ in labels

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,9 +2,7 @@ export { default as regexes } from './regexes';
 export { default as messages } from './messages';
 
 export function fieldNameToLabelConverter(fieldName: string) {
-	let label = fieldName.replace('-', ' '); // Replace all - with spaces
-	label = label.replace('_', ' '); // Replace all _ with spaces
-	label = label.replace('_', ' '); // Replace all _ with spaces
+	let label = fieldName.replace(/[-_]/g, ' '); // Replace all - and _ with spaces
 	label = label.replace(/([A-Z])/g, ' $1'); // 'camelCaseText' to 'camel Case Text' converter
 	label = label.charAt(0).toUpperCase() + label.slice(1); // uppercase first char
 	return label;
